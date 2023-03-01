81e8215f952171d0226d0e21dc8978bf6e2b8f27
Rithubeth G <rithubetham@gmail.com>
2023-03-01 09:25:40 +0530

    test folder

--- tests/tq4.py
+++ tests/tq4.py
@@ -1,6 +1,9 @@
+from datetime import date
 print("Enter the username:")
 username = input()
 print("Enter the year of birth:")
+today = date.today()
+year = today.year
 age = int(input())
-x = 2023 - age
+x = year - age
 print("Hi", username,", you are", x , "years old.")
