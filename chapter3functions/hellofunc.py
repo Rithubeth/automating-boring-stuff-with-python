07881ab85a59c053d25be3ec47ae17d049d5c055
Rithubeth G <rithubetham@gmail.com>
2023-03-02 14:24:51 +0530

    chapter 3

---
+++ chapter3functions/hellofunc.py
@@ -0,0 +1,16 @@
+def hello():
+ print('Howdy!')
+ print('Howdy!!!')
+ print('Hello there.')
+hello()
+hello()
+hello()
+
+def yahoo():
+ print('Yay')
+ print('wow')
+ print('yeah')
+ print('okay') 
+yahoo()
+yahoo()
+yahoo()
